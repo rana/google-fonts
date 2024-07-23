70e477eff95037b3ada90d123291f83c00b7a423
Rana Ian <rana.ian@gmail.com>
2024-07-22 20:00:41 -0700

    Revise serialization

--- fnt/examples/serde.rs
+++ fnt/examples/serde.rs
@@ -1,19 +1,19 @@
 use google_fonts::Font;
-use serde_json::json;
+use serde::{Deserialize, Serialize};
 
 fn main() {
-    let doc1 = Document {
+    let doc1 = Doc {
         font: Font::LemonadaRegular,
         text: "Hello, Doc!".into(),
     };
-    let json_str = json!(doc1);
+    let json_str = serde_json::to_string(&doc1).unwrap();
     eprintln!("{}", json_str);
-    let doc2: Document = serde_json::from_str(&json_str).unwrap();
+    let doc2: Doc = serde_json::from_str(&json_str).unwrap();
     assert_eq!(doc1, doc2)
 }
 
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
-pub struct Document {
+pub struct Doc {
     pub font: Font,
     pub text: String,
 }
